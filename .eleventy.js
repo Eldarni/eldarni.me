739475dd04ad26769e106e6ec38affe148b45e59
RickBaby <richard@smith-net.org.uk>
2021-10-17 19:02:00 +0100

    Build out the basic theme, add scss support - and the first post

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,46 @@
+
+//
+const fs = require('fs');
+const glob = require('glob');
+const matter = require('gray-matter');
+
+//
 module.exports = function(eleventyConfig) {
+
+    //copy over any font files
+    eleventyConfig.addPassthroughCopy('src/fonts');
+
+    //copy over any images
+    eleventyConfig.addPassthroughCopy('src/images');
+
+    //use an after build event to create a copy of the latest post to be used as the homepage
+    eleventyConfig.on('afterBuild', () => {
+
+        //get the post date from the front-matter on each template
+        const posts = glob.sync('./src/posts/**/*.njk').reduce((posts, post) => {
+            const file = matter(fs.readFileSync(post));
+            return [ ...posts, { 'filename' : post, 'date' : new Date(file.data.date) }];
+        }, []);
+
+        //get the latest post (by it's date front-matter)
+        const latestPost = posts.sort((a, b) => b.date - a.date).at(0);
+
+        //to get the filename of the post's artifact = convert the "./src/" to "./public/" and ".njk" to "/index.html"
+        const latestPostArtifactFilename = latestPost.filename.replace('./src/', './public/').replace('.njk', '/index.html')
+        
+        fs.copyFile(latestPostArtifactFilename, './public/index.html', (err) => {
+            if (err) throw err;
+            console.log(`[11ty] Cloned ${latestPostArtifactFilename} as './public/index.html'`);
+        });
+
+    });
+
+    //define the default input and output folders
     return {
         dir: {
-            input: './src/',
+            input: './src',
             output: './public/'
         }
     };
+
 };
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Blog
+# eldarni.me
+
+Simple static site via [Eleventy](https://www.11ty.io), deploying to [Netlify](https://jamesdoc.com).
+
+## Getting going (dev)
+
+- `npm install` - Install dependencies
+- `npm run start` - Build and serve the site locally
+
+## Deploying
+
+- `npm run build` - Build the production assets
